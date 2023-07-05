3c261466700ea084f34e1fc2969ff64fc9fbf57b
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-05 10:49:57 -0400

    create gitignore file and add env file

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from flask import make_response
+
 
 
 app = Flask(__name__)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return make_response("HOWDY")
+
+api.add_resource(Home, '/')
 
 
 if __name__ == '__main__':
