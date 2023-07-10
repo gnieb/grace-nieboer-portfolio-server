14f4e928d3384fd8119aea9bb90658c3c2a047d0
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-10 00:15:56 -0400

    config setup

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask import make_response
+from config import api, app, db
 
 
 
-app = Flask(__name__)
-api = Api(app)
+
 
 class Home(Resource):
     def get(self):
---
+++ config.py
@@ -0,0 +1,12 @@
+from flask_restful import Api
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/jobwave"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
