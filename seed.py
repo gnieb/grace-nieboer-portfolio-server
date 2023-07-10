aeecfac2d61ca96ec089b91a79009c33d47a64f8
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-10 16:29:02 -0400

    seed file added two entries

--- seed.py
+++ seed.py
@@ -1,9 +1,37 @@
 from models import Job, Quote
-from config import db
+from config import db, app
 
 
 def seed():
-    pass
+
+    print("deleting tables")
+    Job.query.delete()
+    Quote.query.delete()
+
+    print("creating tables")
+    db.create_all()
+
+    q1 = Quote(quote='Lorem Ipsum', by='unknown')
+    q2 = Quote(quote='You can do this', by='Kimmy')
+    
+    db.session.add_all([q1, q2])
+    db.session.commit() 
+
+    print("done seeding!")
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        print("Starting seed...")
+        seed()
+
+
+
+
+
+    
 
 
 
