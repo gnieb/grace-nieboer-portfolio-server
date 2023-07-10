6b61080297da3be2004a9433349c91f3acf9db08
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-10 00:22:06 -0400

    jobs and quote models built

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
 from config import db
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Job(db.Model, SerializerMixin):
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    company = db.Column(db.String)
+
+class Quote(db.Model, SerializerMixin):
+    __tablename__ = 'quotes'
+
+    id = db.Column(db.Integer, primary_key = True)
+    quote = db.Column(db.String, nullable = False)
+    by = db.Column(db.String, nullable = False)
+
+    
