41d4f8771595ab327b0a7cda257a18d065d4a04e
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-12 14:17:29 -0400

    design get route for quotes in REST API

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ from models import Job, Quote
 class Home(Resource):
     def get(self):
         return make_response("HOWDY")
+
+
+class Quotes(Resource):
+    def get(self):
+        quotes = [q.to_dict() for q in Quote.query.all()]
+        return make_response(quotes, 200)
+
     
 #custom route for webscraper
 @app.route('/scrape')
@@ -18,6 +25,7 @@ def getjobs():
     
 
 api.add_resource(Home, '/')
+api.add_resource(Quotes, '/quotes')
 
 
 if __name__ == '__main__':
