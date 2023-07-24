2139d8bfd47936743ceb0df936498705bb0a6826
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-23 22:47:27 -0400

    config databaseURI

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from flask_restful import Api
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+import os
 
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gracenieboer:postgres@localhost:5432/jobwave"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
