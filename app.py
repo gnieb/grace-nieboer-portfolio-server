2a61f5b0f173a02b2ca9bc151a2602ca795327ad
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-24 17:09:40 -0400

    add ToDoById api route with patch and get methods allowed

--- app.py
+++ app.py
@@ -36,6 +36,31 @@ class ToDos(Resource):
     def get(self):
         tasks = [t.to_dict() for t in ToDo.query.all()]
         return make_response(tasks, 200)
+    
+class ToDoById(Resource):
+    def get(self, id):
+        task = ToDo.query.filter_by(id=id).first()
+        if not task:
+            return make_response({"error":"ToDo Not Found!"}, 404)
+        return make_response(task.to_dict(), 200)
+
+
+    def patch(self, id):
+        task = ToDo.query.filter_by(id = id).first()
+        if not task:
+            return make_response({"error":"ToDo Not Found"}, 404)
+        try:
+            data = request.get_json()
+            for key in data.keys():
+                setattr(task, key, data[key])
+        except:
+            return make_response({"error":"Validation error, unprocessable entity"}, 422)
+        try:
+            db.session.add(task)
+            db.session.commit()
+        except:
+            return make_response({"error":"Validation error, unprocessable entity"}, 422)
+        return make_response(task.to_dict(), 200)
 
 @app.route('/webhook', methods=["POST"])
 def hook():
@@ -52,6 +77,7 @@ def getjobs():
 api.add_resource(Home, '/')
 api.add_resource(Quotes, '/quotes')
 api.add_resource(ToDos, '/todos' )
+api.add_resource(ToDoById, '/todos/<int:id>')
 
 
 if __name__ == '__main__':
